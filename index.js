23912bf2744a7f7d8bfb58b2f530559c01690d84
Theideabased <ogunmusireseyi01@gmail.com>
2024-10-13 15:14:17 +0100

    just starting with node js but i have to start with javascript first

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("This is nodejs for beginners");
+console.log("this is the first tutorial on nodejs")
+console.log("we will cover nodejs in command line")
+// process.exit(0);
+process.exitCode = 1;
